1c681e4e351d16c8738006b8fbdfff23ff92b8e0
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-15 20:10:14 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, 'M', 'Brown', 'Happy', true);
